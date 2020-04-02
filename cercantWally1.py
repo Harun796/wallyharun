9a24c26f1392662c9583b53f970994e4ad2b3124
Harun796 <harun.meraga@iesjoandaustria.org>
2020-04-02 08:57:24 +0000

    wwww

--- cercantWally1.py
+++ cercantWally1.py
@@ -1,37 +1,35 @@
 import string
 import random
-
-#creació de la llista de simbols
+simbols = []
 #https://docs.python.org/2/library/string.html
 simbols=list(string.printable)
-
 def initLandscape(landscape):
-    for i in range(100):
-        landscape.append("")
-    return landscape
-
-def populateLandscape(landscape):
-    wally = False
-    for posicio in range(100):
-        personatge = random.choice(simbols)
-        if (personatge == "W"):
-            if ("W" not in landscape):
-                wally = True
-                landscape[posicio] = personatge
-        else:    
-            landscape[posicio] = personatge
-    
-    if (not wally):
-        #Assegurem que hi hagi en Wally
-        landscape[random.randrange(100)] ="W"    
-    return landscape
-
-def whereIsWally(landscape,toFind):
-    posicio = -1
-    #TODO
-    #Completeu aquí el codi per fer la cerca de W (ally)
-    return posicio
-
-landscape=populateLandscape(initLandscape([]))
-print(landscape)
-print("Wally està en la posició "+str(whereIsWally(landscape,"W")))
+  for i in range(100):
+      landscape.append("")
+  return landscape
+def populateLandscape(landscape,personatges):
+  wally = False
+  for posicio in range(100):
+      personatge = random.choice(simbols)
+      if (personatge == personatges):
+          if (personatges not in landscape):
+              wally = True
+              landscape[posicio] = personatge
+      else:  
+          landscape[posicio] = personatge
+   if (not wally):
+    if (personatge == personatges):
+      landscape[random.randrange(100)] =personatges  
+  return landscape
+def whereIsCharacter(landscape,toFind):
+  posicio = -1
+  for f in landscape:
+    posicio = posicio + 1
+    if(f == toFind):
+      return posicio
+  return -1
+ 
+personatges= input("Quin personatge vols trobar")
+paisatge=populateLandscape(initLandscape([]),personatges)
+print(paisatge)
+print("El personatge  està en la posició "+str(whereIsCharacter(paisatge,personatges)))
